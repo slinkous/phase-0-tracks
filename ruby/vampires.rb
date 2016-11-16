f5ec996d5f3a8696a0be8f05f21136baf03833f2
slinkous <sralinkous@gmail.com>
2016-11-16 14:51:21 -0500

    Add crude interpretation of y/n responses as boolean

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,11 +1,29 @@
 puts "What's your name?"
 name = gets.chomp
+
 puts "How old are you?"
 age = gets.chomp
+
 puts "What year were you born?"
 birth_year = gets.chomp
-puts "Would you like some garlic bread?"
+
+puts "Would you like some garlic bread? (Y/N)"
 garlic_bread_answer = gets.chomp
-puts "Would you like to enroll in company health insurance?"
+garlic_bread = false
+if garlic_bread_answer == "Y"
+	garlic_bread = true
+end
+
+puts "Would you like to enroll in company health insurance? (Y/N)"
 insurance_answer = gets.chomp
+insurance = false
+if insurance_answer == "Y"
+	insurance = true
+end
+
+
+wolves_like_sunshine = true
+wolves_like_garlic = true
+vampires_like_sunshine = false
+vampires_like_garlic = false
 
