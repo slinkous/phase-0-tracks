7fc111d59f34394f80f4af6e620136e00003b341
slinkous <sralinkous@gmail.com>
2016-11-16 13:54:52 -0500

    Add basic questionaire

---
+++ ruby/vampires.rb
@@ -0,0 +1,11 @@
+puts "What's your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.chomp
+puts "What year were you born?"
+birth_year = gets.chomp
+puts "Would you like some garlic bread?"
+garlic_bread_answer = gets.chomp
+puts "Would you like to enroll in company health insurance?"
+insurance_answer = gets.chomp
+
