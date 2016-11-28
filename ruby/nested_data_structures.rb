c0c15fc6700d03ae662d5280e83af3683253b393
slinkous <sralinkous@gmail.com>
2016-11-28 16:32:11 -0500

    Access nested info with keys and indexes

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -35,4 +35,10 @@ crystal_gems = {
 
 puts crystal_gems
 
+puts crystal_gems[:pearl][:name] + "'s powers are: "
+puts crystal_gems[:pearl][:powers]
 
+puts crystal_gems[:amethyst][:name] + "'s most important power is: "
+puts crystal_gems[:amethyst][:powers][0]
+
+puts crystal_gems[:garnet][:name] + " is a fusion of " + crystal_gems[:garnet][:fusion_members][0] + " and " + crystal_gems[:garnet][:fusion_members][1]
