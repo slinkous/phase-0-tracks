dc74ab96e3500b956d7cafd2c5998185654522f4
slinkous <sralinkous@gmail.com>
2016-11-28 17:53:42 -0500

    Make methods to swap names and shift vowels

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -8,5 +8,33 @@ end
 puts switch_name(real_name)
 
 
+def shift_vowels(name)
+	vowels = "aeiou"
+	name_array = name.downcase.split("")
+	shifted_name =""
+	name_array.each do |letter|
+		if letter.count(vowels) == 1
+			if letter == "u"
+				shifted_name << "a"
+			else
+				shifted_name << vowels[vowels.index(letter) + 1]
+			end
+		else
+			if letter.next.count(vowels) == 1
+				shifted_name << letter.next.next
+			elsif letter == "z"
+				shifted_name << "b"
+			else
+				shifted_name << letter.next
+			end
+		end
+	end
+	shifted_name.capitalize	
+end
+
+puts shift_vowels("AdReiouz")
+
+
+
 
 
