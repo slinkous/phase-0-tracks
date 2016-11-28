48af10d3d86745261fe295943ddc496094b9ae87
slinkous <sralinkous@gmail.com>
2016-11-28 16:27:07 -0500

    Create crystal gems hash

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,38 @@
+#The Crystal Gems
+
+crystal_gems = {
+	garnet: {
+		name: "Garnet",
+		powers: [
+			"future vision",
+			"super gloves"
+			],
+		fusion: true,
+		fusion_members: [
+			"ruby",
+			"sapphire",
+			],
+	},
+	amethyst: {
+		name: "Amethyst",
+		powers: [
+			"magic whip",
+			"shape shifting"
+			],
+		fusion: false,
+		fusion_members: [],
+	},
+	pearl: {
+		name: "Pearl",
+		powers: [
+			"magic spear",
+			"sword fighting"
+			],
+		fusion: false,
+		fusion_members: [],
+	},
+}
+
+puts crystal_gems
+
+
