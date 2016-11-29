8ede239b3beb6ef0add24de5c56d93cc34ea98fe
slinkous <sralinkous@gmail.com>
2016-11-29 17:36:31 -0500

    Create fibonacci method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -10,12 +10,42 @@ def search_array(arr, check_key)
 		end
 	end
 	if i == arr.length
-		i = -1
+		i = nil
 	end
 	i
 end
 
 puts search_array(numbas, 2)
+
+# Create fibonacci sequence
+=begin
+Takes an array of 0 and 1, adds last two digits
+=end 
+def fib(size)
+	i = 0
+	sequence = [0,1]
+	if size == 0
+		sequence = []
+	elsif size == 1
+		sequence = [0]
+	else
+		while i < size - 2
+			sequence << (sequence[-2] + sequence[-1])
+			i += 1
+		end
+	end
+	sequence
+end
+
+puts fib(2)
+puts fib(0)
+puts fib(1)
+puts fib(10)
+puts fib(20)
+puts fib(100)
+
+puts fib(100)[-1] == 218922995834555169026
+	
 	
 
 
