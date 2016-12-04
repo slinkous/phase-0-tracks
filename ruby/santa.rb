6f65d155bfde20b4ee86870d8ccb79405c6a1d79
slinkous <sralinkous@gmail.com>
2016-12-04 16:54:07 -0500

    Add and test attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,18 @@
 class Santa 
-	def initialize
+	def initialize (name, gender = "none", ethnicity = "prefer not to say")
+		@name = name
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0 
 		puts "Initializing Santa instance . . ."
+	end
+	def attribute_test
+		puts @name 
+		puts @gender
+		puts @ethnicity
+		puts @reindeer_ranking
+		puts @age
 	end
 	def speak
 		puts "Ho ho ho! Haaaappy holidays!"
@@ -10,8 +22,12 @@ class Santa
 	end
 end
 
-kris_kringle = Santa.new
+kris_kringle = Santa.new("Saint Nick", "other", "nonbinary")
+kris_kringle.attribute_test
 
-kris_kringle.speak
 
-kris_kringle.eat_milk_and_cookies("chocolate chip")
+#kris_kringle.speak
+
+#kris_kringle.eat_milk_and_cookies("chocolate chip")
+
+
