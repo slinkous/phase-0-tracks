c52574c6f49ed95d1b971682490a14a0dfb3fc55
slinkous <sralinkous@gmail.com>
2016-12-04 21:46:53 -0500

    Use mixin module methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,11 +1,41 @@
 module Shout
-	def self.yell_angrily(words)
+	def yell_angrily(words)
 		words + "!!!! :("
 	end
-	def self.yell_happily(words)
+	def yell_happily(words)
 		words + "!!!! :)"
-	end	
+	end
 end
 
-puts Shout.yell_happily("This is great")
-puts Shout.yell_angrily("This is not")
+class Student
+	include Shout
+end
+
+class Teacher
+	include Shout
+end
+
+a_teacher = Teacher.new
+
+puts a_teacher.yell_angrily("Quiet please")
+
+a_student = Student.new
+
+puts a_student.yell_happily("We get to learn today")
+
+
+
+
+
+
+#module Shout
+#	def self.yell_angrily(words)
+#		words + "!!!! :("
+#	end
+#	def self.yell_happily(words)
+#		words + "!!!! :)"
+#	end	
+#end
+
+#puts Shout.yell_happily("This is great")
+#puts Shout.yell_angrily("This is not")
